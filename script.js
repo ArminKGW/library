a99e57eaa52f7a9d94da2639d8972e5892d12ca5
Armin <160379151+ArminKGW@users.noreply.github.com>
2024-05-16 05:59:09 +0200

    Make "+ Add book" button functional with dialog
    The button is not functional. by pressing on the button, a
    modal box will pop up, in which user will fill a form and submit it.
    The information about the book and whether it's been read or not is saved
    and will be shown on a card on the screen.

--- script.js
+++ script.js
@@ -1,42 +1,58 @@
 const myLibrary = [];
 const container = document.querySelector(".container");
 const cardContainer = document.querySelector(".card-container");
+const addBookBtn = document.querySelector(".add-book button");
+const dialog = document.querySelector("dialog");
+const inputTitle = dialog.querySelector("#book-title");
+const inputAuthor = dialog.querySelector("#book-author");
+const inputPages = dialog.querySelector("#book-page-number");
+const submitBtn = dialog.querySelector("dialog button");
+const checkbox = dialog.querySelector('input[type="checkbox"]');
+let title = "";
+let author = "";
+let pages = "";
+let isRead = false;
+addBookBtn.addEventListener("click", () => {
+    dialog.showModal();
+});
 
-addBookToLibrary(myLibrary);
-displayBook(myLibrary);
-
-
-const readButtons = document.querySelectorAll(".read, .not-read");
-readButtons.forEach((item) => {
-    if(item.classList.contains("read")){
-        item.textContent = "Read";
-    }
-    else if(item.classList.contains("not-read")){
-        item.textContent = "Not read";
+submitBtn.addEventListener("click", (e) => {
+    if(dialog.querySelector("form").checkValidity()){
+        e.preventDefault();
+        title = inputTitle.value;
+        author = inputAuthor.value;
+        pages = inputPages.value;
+        isRead = checkbox.checked;
+        addBookToLibrary(myLibrary, title, author, pages, isRead);
+        dialog.close();
+        resetForm();
+        displayBook(myLibrary);
     }
 });
-readButtons.forEach((item) => {
-    item.addEventListener("click", (event) => {
-        item.classList.toggle("read");
-        item.classList.toggle("not-read");
-        if(item.classList.contains("read")){
-            item.textContent = "Read";
+
+cardContainer.addEventListener("click", (e) => {
+    if(e.target.classList.contains("read") || e.target.classList.contains("not-read")){
+        const readCard = e.target.closest(".card");
+        const title = readCard.querySelector(".title").textContent.substring(7);
+        if(e.target.classList.contains("read")){
+            changeReadState(title, myLibrary);
+            e.target.textContent = "Not read"
         }
         else{
-            item.textContent = "Not read";
+            changeReadState(title, myLibrary);
+            e.target.textContent = "Read";
         }
-    });
-});
-
-
-const removeButtons = document.querySelectorAll(".remove");
-removeButtons.forEach((item) => {
-    item.addEventListener("click", (event) => {
-        const removeCard = item.closest(".card");
+        e.target.classList.toggle("read");
+        e.target.classList.toggle("not-read");
+    }
+    else if(e.target.classList.contains("remove")){
+        const removeCard = e.target.closest(".card");
         if(removeCard){
+            const title = removeCard.querySelector(".title").textContent.substring(7);
+            removeFromLibrary(title, myLibrary);
             removeCard.remove();
         }
-    });
+    }
 });
 
 function Book(title, author, pagesNumber, isRead){
@@ -47,51 +63,67 @@ function Book(title, author, pagesNumber, isRead){
 }
 
 
-function addBookToLibrary(library){
-    const book1 = new Book("book1", "man", 100, true);
-    const book2 = new Book("book2", "ali", 200, false);
-    const book3 = new Book("book3", "sepehr", 300, true);
-    const book4 = new Book("book4", "sobhan", 400, true);
-    const book5 = new Book("book5", "majid", 500, false);
-    library.push(book1);
-    library.push(book2);
-    library.push(book3);
-    library.push(book4);
-    library.push(book5);
+function addBookToLibrary(library, title, author, pagesNumber, isRead){
+    const book = new Book(title, author, pagesNumber, isRead);
+    library.push(book);
 }
 
 function displayBook(library){
-    for(let i = 0; i < library.length; i++){
-        const card = document.createElement("div");
-        card.classList.add("card");
-        const book = document.createElement("div");
-        book.classList.add("book");
-        const title = document.createElement("div");
-        title.classList.add("title");
-        const author = document.createElement("div");
-        author.classList.add("author");
-        const pagesNumber = document.createElement("div");
-        pagesNumber.classList.add("pages-number");
-        const buttons = document.createElement("div");
-        buttons.classList.add("buttons");
-        const isRead = document.createElement("button");
-        if(library[i].isRead === true){
-            isRead.classList.add("read");
-        }
-        else if(library[i].isRead === false){
-            isRead.classList.add("not-read");
-        }
-        const remove = document.createElement("button");
-        remove.classList.add("remove");
-        title.textContent = "Title: " + library[i].title;
-        author.textContent = "Author: " + library[i].author;
-        pagesNumber.textContent = "Number of Pages: " + library[i].pagesNumber;
-        remove.textContent = "Remove";
-        book.append(title, author, pagesNumber);
-        buttons.append(isRead, remove);
-        card.append(book, buttons);
-        cardContainer.appendChild(card);
+    const len = library.length;
+    const card = document.createElement("div");
+    card.classList.add("card");
+    const book = document.createElement("div");
+    book.classList.add("book");
+    const title = document.createElement("div");
+    title.classList.add("title");
+    const author = document.createElement("div");
+    author.classList.add("author");
+    const pagesNumber = document.createElement("div");
+    pagesNumber.classList.add("pages-number");
+    const buttons = document.createElement("div");
+    buttons.classList.add("buttons");
+    const isRead = document.createElement("button");
+    if(library[len-1].isRead === true){
+        isRead.classList.add("read");
+        isRead.textContent = "Read";
     }
+    else if(library[len-1].isRead === false){
+        isRead.classList.add("not-read");
+        isRead.textContent = "Not read";
+    }
+    const remove = document.createElement("button");
+    remove.classList.add("remove");
+    title.textContent = "Title: " + library[len-1].title;
+    author.textContent = "Author: " + library[len-1].author;
+    pagesNumber.textContent = "Number of Pages: " + library[len-1].pagesNumber;
+    remove.textContent = "Remove";
+    book.append(title, author, pagesNumber);
+    buttons.append(isRead, remove);
+    card.append(book, buttons);
+    cardContainer.appendChild(card);
+}
+
+function changeReadState(title, library){
+    library.forEach((book) => {
+        if(book.title === title){
+            book.isRead = !(book.isRead);
+        }
+    });
+}
+
+function removeFromLibrary(title, library){
+    library.forEach((book, index) => {
+        if(book.title === title){
+            library.splice(index, 1);
+        }
+    });
+}
+
+function resetForm(){
+    inputTitle.value = "";
+    inputAuthor.value = "";
+    inputPages.value = "";
+    checkbox.checked = false;
 }
 
 
